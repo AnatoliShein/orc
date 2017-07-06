d525f3908b275ef9d2e01611e9356930f2c80a10
Anatoli Shein <anatoli.shein@hpe.com>
2017-07-06 09:43:05 -0400

    Removed !fs notation and fixed line wrapping

--- c++/src/OrcHdfsFile.cc
+++ c++/src/OrcHdfsFile.cc
@@ -54,57 +54,66 @@ namespace orc {
       }
 
       hdfs::Options options;
-      //Setting the config path to the default: "$HADOOP_CONF_DIR" or "/etc/hadoop/conf"
+      //Setting conf path to default: "$HADOOP_CONF_DIR" or "/etc/hadoop/conf"
       hdfs::ConfigurationLoader loader;
-      //Loading default config files core-site.xml and hdfs-site.xml from the config path
-      hdfs::optional<hdfs::HdfsConfiguration> config = loader.LoadDefaultResources<hdfs::HdfsConfiguration>();
-      //TODO: HDFS-9539 - after this is resolved, valid config will always be returned.
+      //Loading configs core-site.xml and hdfs-site.xml from the config path
+      hdfs::optional<hdfs::HdfsConfiguration> config =
+          loader.LoadDefaultResources<hdfs::HdfsConfiguration>();
+      //TODO: HDFS-9539 - when resolved, valid config will always be returned
       if(config){
         //Loading options from the config
         options = config->GetOptions();
       }
       hdfs::IoService * io_service = hdfs::IoService::New();
       //Wrapping file_system into a unique pointer to guarantee deletion
-      file_system = std::unique_ptr<hdfs::FileSystem>(hdfs::FileSystem::New(io_service, "", options));
-      if (!file_system) {
+      file_system = std::unique_ptr<hdfs::FileSystem>(
+          hdfs::FileSystem::New(io_service, "", options));
+      if (file_system.get() == nullptr) {
         throw ParseError("Can't create FileSystem object. ");
       }
       hdfs::Status status;
-      //Check if the user supplied the host
+      //Checking if the user supplied the host
       if(!uri.value().get_host().empty()){
-        //If port is supplied we use it, otherwise we use the empty string so that it will be looked up in configs.
-        std::string port = (uri.value().get_port()) ? std::to_string(uri.value().get_port().value()) : "";
+        //Using port if supplied, otherwise using "" to look up port in configs
+        std::string port = (uri.value().get_port()) ?
+            std::to_string(uri.value().get_port().value()) : "";
         status = file_system->Connect(uri.value().get_host(), port);
         if (!status.ok()) {
-          throw ParseError("Can't connect to " + uri.value().get_host() + ":" + port + ". " + status.ToString());
+          throw ParseError("Can't connect to " + uri.value().get_host()
+              + ":" + port + ". " + status.ToString());
         }
       } else {
         status = file_system->ConnectToDefaultFs();
         if (!status.ok()) {
           if(!options.defaultFS.get_host().empty()){
-            throw ParseError("Error connecting to " + options.defaultFS.str() + ". " + status.ToString());
+            throw ParseError("Error connecting to " +
+                options.defaultFS.str() + ". " + status.ToString());
           } else {
-            throw ParseError("Error connecting to the cluster: defaultFS is empty. " + status.ToString());
+            throw ParseError(
+                "Error connecting to the cluster: defaultFS is empty. "
+                + status.ToString());
           }
         }
       }
 
-      if (!file_system) {
+      if (file_system.get() == nullptr) {
         throw ParseError("Can't connect the file system. ");
       }
 
       hdfs::FileHandle *file_raw = nullptr;
       status = file_system->Open(uri->get_path(), &file_raw);
       if (!status.ok()) {
-        throw ParseError("Can't open " + uri->get_path() + ". " + status.ToString());
+        throw ParseError("Can't open "
+            + uri->get_path() + ". " + status.ToString());
       }
-      //wrapping file_raw into a unique pointer to guarantee deletion
+      //Wrapping file_raw into a unique pointer to guarantee deletion
       file.reset(file_raw);
 
       hdfs::StatInfo stat_info;
       status = file_system->GetFileInfo(uri->get_path(), stat_info);
       if (!status.ok()) {
-        throw ParseError("Can't stat " + uri->get_path() + ". " + status.ToString());
+        throw ParseError("Can't stat "
+            + uri->get_path() + ". " + status.ToString());
       }
       totalLength = stat_info.length;
     }
@@ -132,7 +141,9 @@ namespace orc {
       size_t last_bytes_read = 0;
       
       do {
-        status = file->PositionRead(buf, static_cast<size_t>(length) - total_bytes_read, static_cast<off_t>(offset + total_bytes_read), &last_bytes_read);
+        status = file->PositionRead(buf,
+            static_cast<size_t>(length) - total_bytes_read,
+            static_cast<off_t>(offset + total_bytes_read), &last_bytes_read);
         if(!status.ok()) {
           throw ParseError("Error reading the file: " + status.ToString());
         }
